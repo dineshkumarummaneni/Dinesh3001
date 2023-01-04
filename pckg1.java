0040cf14d5c940d16120022140cb154db1f0d6bf
dineshkumarummaneni <102919419+dineshkumarummaneni@users.noreply.github.com>
2023-01-04 15:08:52 +0530

    hii

---
+++ pckg1.java
@@ -0,0 +1,13 @@
+package Pckg1;
+public class pckgl{
+int a=1;
+private int pri_a=2;
+protected int pro_a=3;
+public int pub_a=4;
+public Pckg1() {
+System.out.println(pri_a);
+System.out.println(pro_a);
+System.out.println(pub_a);
+System.out.println(a);
+}
+}
